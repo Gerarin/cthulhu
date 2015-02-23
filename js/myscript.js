738719a2fd6fa8c1dbb55f692ec8dfa961dccdc1
Gerald <gerald@athelas.pe>
2015-02-23 16:57:53 -0500

    paginas 1 3 5

---
+++ js/myscript.js
@@ -0,0 +1,31 @@
+///////////////pagina de inicio//////////////////
+
+1	al dar click al boton "create your investigator" se debe abrir la siguiente pagina: "core rolls"
+2 al dar click al nombre de un personaje creado, se debe abrir la "pagina principal" de dicho personaje
+3	moviendo a derecha (o izquierda) aparecen mas nombres de investigadores (si la cuenta tiene mas de 3).
+
+
+///////////////////roll Calls////////////////
+
+
+
+////////////ocupation///////////////////
+1	al dar hover sobre el boton, aparece la lista de profesiones, al dar click sobre una, esta se selecciona(cambia de color x ejenmplo)
+2	dar click sobre back te regresa a "roll calls"
+3	dar click al boton "next" te lleva a la pagina "ocupational skills"
+
+
+ocupational skills
+
+
+
+
+//////////////hobby skills/////////////////
+
+1	se debe ver la lista completa de skills , en ella su porcentaje base y 2 botones para aumentar o disminuir puntaje
+2	el total de "hobby points" es INT * 10
+		cuando se da click a los botones de adicionar o restar, se va modificando este numero base
+3	cuando se selecciona un skill se puede editar en al cuadro de la derecha		
+4	dar click sobre back te regresa a "ocupational skills"
+5	dar click al boton "next" te lleva a la pagina "investigator data"
+
